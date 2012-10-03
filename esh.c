fa2a5d2bd796754682189f83b8edfb7c4b3dec05
K <kalnajar09@gmail.com>
2012-10-02 22:56:30 -0400

    added basic function to remove a job from the pipeline, check what commands are available and look through a given commandline

--- esh.c
+++ esh.c
@@ -10,6 +10,20 @@
 
 #include "esh.h"
 
+//function definitions
+static void execute_command_line (struct esh_command_line *command_line);
+static int  esh_commands 		 (char **argv);
+static void remove_job   		 (struct esh_pipeline *pipe);
+
+/*
+* struct to hold a job for the pipe
+*/
+struct job
+{
+	struct list_elem elem;
+	struct esh_pipeline *pipe;
+};
+
 static void
 usage(char *progname)
 {
@@ -72,6 +86,9 @@ main(int ac, char *av[])
     int opt;
     list_init(&esh_plugin_list);
 
+	//init list to track jobs
+	list_init(&jobs_list);
+
     /* Process command-line arguments. See getopt(3) */
     while ((opt = getopt(ac, av, "hp:")) > 0) {
         switch (opt) {
@@ -111,4 +128,85 @@ main(int ac, char *av[])
         esh_command_line_free(cline);
     }
     return 0;
+}
+
+/*
+* Checks if the command passed can be handled by the esh shell.
+* If it is not a builtin command, returns 0.
+*/
+static int esh_commands(char **argv)
+{
+	
+	if(!strcmp(argv[0], "exit"))
+	{
+		exit(0);
+	}
+	
+	if(!strcmp(argv[0], "jobs"))
+	{
+		//esh_jobs();		
+		return 1;
+	}
+	
+	if(!strcmp(argv[0], "fg"))
+	{
+		//esh_fg(argv);
+		return 2;
+	}
+	
+	if(!strcmp(argv[0], "bg"))
+	{
+		//esh_bg(argv);
+		return 3;
+	}
+	
+	if(!strcmp(argv[0], "kill"))
+	{
+		//esh_kill(argv);
+		return 4;
+	}
+	
+	if(!strcmp(argv[0], "stop"))
+	{
+		//esh_stop(argv);
+		return 5;
+	}
+
+	return 0;
+}
+
+/*
+* Executes all pipelines included in esh_command_line.
+*/
+static void execute_command_line(struct esh_command_line *command_line)
+{
+	struct list_elem * e = list_begin (&command_line->pipes); 
+
+    for (; e != list_end (&command_line->pipes); e = list_next (e)) 
+	{
+        struct esh_pipeline *pipe = list_entry(e, struct esh_pipeline, elem);
+
+		//execute the pipeline
+		esh_execute_pipe(pipe);
+    }
+}
+
+/*
+* Remove a job from the job list pipeline.
+*/
+static void remove_job(struct esh_pipeline *pipeline)
+{
+	struct list_elem *e = list_begin(&jobs_list);
+		
+	for(; e != list_end(&jobs_list); e = list_next(e))
+	{
+		struct job *j = list_entry(e, struct job, elem);
+			
+		if(j->pipe->pgrp == pipeline->pgrp)
+		{
+			list_remove(e);
+			return;
+		}
+	}
+	return;
 }
